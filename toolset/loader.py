3f2c05d6a65e3d84e549536448b97b3120ee836e
Sandeep V Kollipara <sandeep.kollipara@icloud.com>
2025-03-31 14:41:23 +0000

    Added the folder structure and base files

---
+++ toolset/loader.py
@@ -0,0 +1,49 @@
+import pandas as pd
+from pydantic import BaseModel, Field
+from langchain.tools import tool
+
+
+class FileName(BaseModel):
+    filename: str = Field(..., description="Filename of the data file to be loaded")
+
+@tool(args_schema=FileName)
+def load_csv(filename: str) -> dict:
+    """Check if .CSV file is present, load it into the dataframe and return it"""
+    try:
+        df = pd.read_csv(r'/bb/'+filename)
+    except:
+        return 'Error loading file'
+    return f'Verified the file as CSV and loaded it: {df}'
+
+
+@tool(args_schema=FileName)
+def load_excel(filename: str) -> dict:
+    """Check if .XLS or .XLSX file is present, load it into the dataframe and return it"""
+    try:
+        df = pd.read_excel(r'./bb/'+filename)
+    except:
+        return 'Error loading file'
+    return f'Verified the file as Excel and loaded it: {df}'
+
+@tool(args_schema=FileName)
+def load_parquet(filename: str) -> dict:
+    """Check if .PARQUET file is present, load it into the dataframe and return it"""
+    try:
+        df = pd.read_parquet(r'./bb/'+filename)
+    except:
+        return 'Error loading file'
+    return f'Verified the file as Parquet and loaded it: {df}'
+
+
+
+
+
+@tool(args_schema=FileName)
+def load_tsv(filename: str) -> dict:
+    """Check if .TSV file is present, load it into the dataframe and return it"""
+    try:
+        df = pd.read_csv(r'./bb/'+filename, sep='\t', quoting=3)
+    except:
+        return 'Error loading file'
+    return f'Verified the file as TSV and loaded it: {df}'
+
